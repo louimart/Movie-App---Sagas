1d909041eca0c07e3ccbdfdcf13ccde21e894f0e
Louis Martin <louimart@gmail.com>
2024-03-02 01:04:44 -0600

    OnCLick routes to selected movie card's detail page, needs detail information displayed

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,27 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+// useHistory for routing to Details page onClick
+import { useHistory } from "react-router-dom";
 import './MovieList.css';
 
 function MovieList() {
 
   const dispatch = useDispatch();
   const movies = useSelector(store => store.movies);
+  // history to route to details page onClick
+  const history = useHistory();
 
   useEffect(() => {
     dispatch({ type: 'FETCH_MOVIES' });
   }, []);
+
+const handleClickToDetail = (event)=>{
+  event.preventDefault();
+  console.log('details page for', event.target.alt)
+  alert(`Going to Details, ${event.target.alt}`);
+  // route to the clicked movie via it's id
+  history.push(`/details/${event.target.id}}`);
+}
 
   return (
     <main>
@@ -17,7 +29,7 @@ function MovieList() {
       <section className="movies">
         {movies.map(movie => {
           return (
-            <div data-testid='movieItem' key={movie.id}>
+            <div data-testid='movieItem' key={movie.id} onClick={handleClickToDetail}>
               <h3>{movie.title}</h3>
               <img src={movie.poster} alt={movie.title}/>
             </div>
