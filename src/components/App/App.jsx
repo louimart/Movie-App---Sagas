e8730226c34a0145fb6c7dd992c8e4ace65427dd
Louis Martin <louimart@gmail.com>
2024-03-02 00:14:47 -0600

    initial commit and detail page route created

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,24 @@
 import { Route, HashRouter as Router } from 'react-router-dom';
 import MovieList from '../MovieList/MovieList';
+import Details from '../Details/Details';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
+
         {/* Details page */}
+        <Route path="/details/:id" exact>
+          <Details />
+        </Route>
 
         {/* Add Movie page */}
-        
+
       </Router>
     </div>
   );
