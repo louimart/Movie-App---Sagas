bc80e1829462969e286e1b4a180c8bd4f7a036b3
Louis Martin <louimart@gmail.com>
2024-03-09 07:29:59 -0600

    building out the form with textarea and dropdown selector

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -3,20 +3,46 @@ import { useHistory } from 'react-router-dom';
 
 function AddMovie() {
   return (
-    <>
+    <form>
       <h1>Add a Movie</h1>
-      <form>
-        <input id="add-movie" placeholder="title" type="text" />
-        <input id="add-movie" placeholder="movie poster image URL" type="text" />
-        <input id="add-movie" placeholder="movie description" type="text" />
-        <input id="add-movie" placeholder="genre" type="text" />
-      </form>
-    </>
+      <section>
+        <label for="title">
+          Title: <input id="title" placeholder="title" type="text" />
+        </label>
+        <br/>
+        <label for="image-url">
+          Movie Poster Image URL: <input id="image-url" placeholder="movie poster image URL" type="text" />
+        </label>
+        <br/>
+        <label for="description">
+          Movie Description: <textarea name="description" id="description" placeholder="description"></textarea>
+        </label>
+      </section>
+      <section>
+        <label for="genres">
+          <span>Genres: </span>
+        </label>
+        <select id="genres">
+          <option value="Adventure">Adventure</option>
+          <option value="Animated">Animated</option>
+          <option value="Biographical">Biographical</option>
+          <option value="Comedy">Comedy</option>
+          <option value="Disaster">Disaster</option>
+          <option value="Drama">Drama</option>
+          <option value="Epic">Epic</option>
+          <option value="Fantasy">Fantasy</option>
+          <option value="Musical">Musical</option>
+          <option value="Romantic">Romantic</option>
+          <option value="Science-Fiction">Science Fiction</option>
+          <option value="Space-Opera">Space-Opera</option>
+          <option value="Superhero">Superhero</option>
+        </select>
+      </section>
+      <section>
+        <button type="submit" id="add-movie-btn">Submit</button>
+      </section>
+    </form>
   );
 }
 
 export default AddMovie;
-
-// - an input field (for the movie poster image URL)
-// - a textarea (for the movie description)
-// - a dropdown (to choose a **single** genres)
